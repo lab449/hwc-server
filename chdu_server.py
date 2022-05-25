019ff6b79b7375a72ad07d7f7e535d4cfd8386fc
TexnoMan <antipov.v.a@yandex.ru>
2022-05-25 17:28:58 +0300

    add writing html on web server

--- chdu_server.py
+++ chdu_server.py
@@ -118,6 +118,8 @@ def client_update():
 if __name__ == '__main__':
     # context = ('hdu2--cacert503.pem', 'localhost.pem')#certificate and key files
     # app.run(debug=True, ssl_context=context, host='127.0.0.1', port=port)
+    os.system('hmd2html -s less ../hwc-matlab-client/README.md -d ../html')
+    os.system('mv ../html/README.html /var/www/hdu/html/index.html')
     app.run(debug=True, host=auth.host, port=auth.port)
 
     
